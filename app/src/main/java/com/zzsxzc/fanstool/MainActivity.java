75d906e58aa880bccb6db631f122bd5c3c589b93
zhengzhaosheng <1350111595@qq.com>
2020-11-23 22:28:17 +0800

    Initial commit

---
+++ app/src/main/java/com/zzsxzc/fanstool/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zzsxzc.fanstool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+*@Author zheng zs
+*@Date 22:18 2020/11/23
+*@Description 
+**/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
